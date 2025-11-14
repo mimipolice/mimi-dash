12a375b0d76cb38d3166ed62c84ddc1fee8f3414
956zs <kupp59371@gmail.com>
2025-11-15 01:04:48 +0800

    style(login): adjust page layout and responsiveness

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,8 +5,8 @@ export default async function Login() {
   const session = await auth();
   if (session?.user) return redirect("/dashboard");
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center px-5 dark:bg-zinc-800">
-      <LoginForm className="w-full md:w-1/2 lg:w-1/3" />
+    <div className="flex min-h-screen flex-col items-center justify-center px-4 py-8 dark:bg-zinc-800">
+      <LoginForm className="w-full max-w-md md:max-w-2xl" />
     </div>
   );
 }
